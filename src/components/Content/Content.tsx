5755ef7ade6a927300997c1ed829b823487a909b
jvduarte17 <j17duarte@hotmail.com>
2022-06-10 20:06:04 -0300

    add animation to image container

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,33 +1,89 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import { Movie } from '../../interfaces/Movie';
+import { getGenreNameById } from '../../utils/genres';
 
 interface ContentProps {
   movie: Movie,
   moviePosterPath: string
 }
 
+interface ImageProps {
+  imageVisibility?: boolean;
+}
+
 export const Content = ({
   movie,
   moviePosterPath
 }: ContentProps) => {
+  const [imageVisibility, setImageVisibility] = useState(true);
+  
+  const genreName = getGenreNameById(movie.genre_ids[0]);
+  const releaseDate = movie.release_date.split('-')[0];
+  const voteAverage = movie.vote_average.toString();
+  const moviePopularity = movie.popularity.toString();
+
+  const isEmptyString = (text: string) => {
+    return text.length === 0;
+  }
+  
+  const movieInfoAttributes = [
+    { 
+      attr: 'GÊNERO',
+      value: !isEmptyString(genreName) ? genreName : 'Não informado'
+    },
+    { 
+      attr: 'ANO',
+      value: !isEmptyString(releaseDate) ? releaseDate : 'Não informado'
+    },
+    {
+      attr: 'MÉDIA (TMDB)',
+      value: !isEmptyString(voteAverage) ? voteAverage : 'Não informado'
+    },
+    {
+      attr: 'POPULARIDADE',
+      value: !isEmptyString(moviePopularity) ? moviePopularity : 'Não informado'
+    }
+  ]
 
   return (
     <Container>
-      <Image src={ moviePosterPath } />
 
-      <MovieInfoContainer>
+      <MovieImageContainer
+        onMouseEnter={() => setImageVisibility(false)}
+        onMouseLeave={() => setImageVisibility(true)}
+      >
+        <MovieImage src={ moviePosterPath }  />
+        <GeneralMovieInfo imageVisibility={ imageVisibility }>
+          {
+            movieInfoAttributes.map((movieAttr, key) => {
+              return (
+                <InnerGeneralMovieInfo key={key}>
+                  <GeneralMovieInfoTitle>{movieAttr.attr}</GeneralMovieInfoTitle>
+                  <GeneralMovieInfoContent>{movieAttr.value}</GeneralMovieInfoContent>
+                </InnerGeneralMovieInfo>
+              )
+            })
+          }
+        </GeneralMovieInfo>
+      </MovieImageContainer>
+
+      <MovieContent>
         <MovieTitle>{ movie.title }</MovieTitle>
         <MovieDescription>
-          { movie.overview }
-        </MovieDescription>
-      </MovieInfoContainer>
+          { movie.overview.length !== 0 ? 
+            movie.overview :
+            'Descrição não informada \u{1F614}'
+          }
+          </MovieDescription>
+      </MovieContent>
     </Container>
   )
 }
 
 const Container = styled.div`
   width: 45rem;
-  height: 16rem;
+  height: 19rem;
   border-radius: 10px;
   display: flex;
   margin: 0 auto;
@@ -37,17 +93,56 @@ const Container = styled.div`
     0px 24px 38px 3px hsla(0,0%,0%,0.14), 
     0px 9px 46px 8px hsla(0,0%,0%,0.12), 
     0px 11px 15px -7px hsla(0,0%,0%,0.2);
+
+  @media(max-width: 740px) {
+    width: 35rem;
+  }
+
+  @media(max-width: 700px) {
+    width: 25rem;
+    height: 20rem;
+  }
+
+  @media(max-width: 410px) {
+    width: 18rem;
+  }
 `
 
-const Image = styled.img`
-  width: 12rem;
+const MovieImageContainer = styled.div`
+  display: flexbox;
+
+  @media(max-width: 740px) {
+    display: none;
+  }
+`
+
+const MovieImage = styled.img`
+  width: 13.5rem;
+  height: 18rem;
   margin: .5rem;
   border-radius: 10px;
-  background-color: white;
+  position: absolute;
 `
 
-const MovieInfoContainer = styled.div`
+const GeneralMovieInfo = styled.div<ImageProps>`
+  width: 13.5rem;
+  margin: .5rem;
+  border-radius: 10px;
+  position: relative;
+  transition: all 350ms ease-out;
+  opacity: ${(p) => p.imageVisibility ? 0 : 1 };
+  color: #FFFFFF;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+
+  background: rgb(19,38,89);
+  background: linear-gradient(0deg, rgba(19,38,89,0.8664508039543942) 0%, rgba(193,42,35,1) 100%);
+`
+
+const MovieContent = styled.div`
   width: 31.5rem;
+  margin-right: .5rem;
 `
 
 const MovieTitle = styled.p`
@@ -66,9 +161,28 @@ const MovieDescription = styled.p`
   color: #FFFCF9;
   margin: 8px;
   overflow-y: auto;
-  height: 12.8rem;
+  height: 15.8rem;
 
   &::-webkit-scrollbar {
     display: none;
   }
+
+  @media(max-width: 700px) {
+    height: 16.4rem;
+  }
+`
+
+const InnerGeneralMovieInfo = styled.div`
+  padding: .7rem;
+  cursor: default;
+`
+
+const GeneralMovieInfoTitle = styled.p`
+  font-weight: 700;
+  margin: 0;
+`
+
+const GeneralMovieInfoContent = styled.p`
+  font-weight: 400;
+  margin: 1px 0 0;
 `
--- src/utils/genres.ts
+++ src/utils/genres.ts
@@ -2,77 +2,107 @@ export const genres = [
   {
     id: 28,
     name: "Action",
+    portugueseName: "Ação"
   },
   {
     id: 12,
     name: "Adventure",
+    portugueseName: "Aventura"
   },
   {
     id: 16,
     name: "Animation",
+    portugueseName: "Animação"
   },
   {
     id: 35,
     name: "Comedy",
+    portugueseName: "Comédia"
   },
   {
     id: 80,
     name: "Crime",
+    portugueseName: "Crime"
   },
   {
     id: 99,
     name: "Documentary",
+    portugueseName: "Documentário"
   },
   {
     id: 18,
     name: "Drama",
+    portugueseName: "Drama"
   },
   {
     id: 10751,
     name: "Family",
+    portugueseName: "Família"
   },
   {
     id: 14,
     name: "Fantasy",
+    portugueseName: "Fantasia"
   },
   {
     id: 36,
     name: "History",
+    portugueseName: "História"
   },
   {
     id: 27,
     name: "Horror",
+    portugueseName: "Terror"
   },
   {
     id: 10402,
     name: "Music",
+    portugueseName: "Musical"
   },
   {
     id: 9648,
     name: "Mystery",
+    portugueseName: "Mistério"
   },
   {
     id: 10749,
     name: "Romance",
+    portugueseName: "Romance"
   },
   {
     id: 878,
     name: "Science Fiction",
+    portugueseName: "Sci-Fi"
   },
   {
     id: 10770,
     name: "TV Movie",
+    portugueseName: "TV Movie"
   },
   {
     id: 53,
     name: "Thriller",
+    portugueseName: "Suspense"
   },
   {
     id: 10752,
     name: "War",
+    portugueseName: "Guerra"
   },
   {
     id: 37,
     name: "Western",
+    portugueseName: "Western"
   },
 ];
+
+export const getGenreNameById = (genreId: number) => {
+  let targetGenre = '';
+
+  genres.forEach(genre => {
+    if (genre.id === genreId)
+      targetGenre = genre.portugueseName;
+  })
+
+  return targetGenre;
+}
